ca11b794dbb7b2dc7ae290ece9cafd12e51ef40f
NikolozMazmishvili26 <125471672+NikolozMazmishvili26@users.noreply.github.com>
2023-03-14 13:24:23 +0400

    multipage-api done

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import {
+  getAllProject,
+  getAppProjects,
+  getGraphicProjects,
+  getWebProjects,
+} from "./controllers/project-controller.js";
+import bodyParser from "body-parser";
+import cors from "cors";
+import connect from "./database/mongo.js";
+import dotenv from "dotenv";
+dotenv.config();
+connect();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/images", express.static("./public/images"));
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "app works!" });
+});
+
+app.get("/api/projects", getAllProject);
+app.get("/api/projects/web", getWebProjects);
+app.get("/api/projects/app", getAppProjects);
+app.get("/api/projects/graphic", getGraphicProjects);
+
+app.listen(process.env.PORT || 3000);
